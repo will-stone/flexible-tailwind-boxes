432f8019223834e6fff0c145af1ae6a92b1d1b58
Will Stone <654103+will-stone@users.noreply.github.com>
2025-03-08 15:33:37 +0000

    Add grid layout

--- src/app.tsx
+++ src/app.tsx
@@ -1,5 +1,13 @@
 import type { FC } from 'react'
 
 export const App: FC = () => {
-  return <div className="text-6xl">Moo</div>
+  return (
+    <div className="grid size-full grid-cols-12 grid-rows-2 gap-px bg-gray-900">
+      <div className="col-span-2 bg-white p-4">moo</div>
+      <div className="col-span-8 bg-white p-4">moo</div>
+      <div className="col-span-2 row-span-2 bg-white p-4">moo</div>
+      <div className="col-span-2 bg-white p-4">moo</div>
+      <div className="col-span-8 bg-white p-4">moo</div>
+    </div>
+  )
 }
