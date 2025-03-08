4bad422980ccf5468111070cb777c2ba2001aa58
Will Stone <654103+will-stone@users.noreply.github.com>
2025-03-08 15:20:45 +0000

    Lint on commit

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,4 +1,4 @@
 export default {
   '*.{css,json,md,js,cjs,mjs,ts,jsx,tsx,astro}': ['prettier --write'],
-  // '*.{js,cjs,mjs,ts,tsx,jsx,astro}': ['eslint --fix'],
+  '*.{js,cjs,mjs,ts,tsx,jsx,astro}': ['eslint --fix'],
 }
